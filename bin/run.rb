878249069253519395fbc2a0cd9be3f23c89c428
github username <github email address>
2017-06-26 16:20:28 -0400

    Added a few gems, config.ru, and a controller file. also commented out runner to prevent program from hanging in the browser

--- bin/run.rb
+++ bin/run.rb
@@ -1,11 +1,11 @@
-require_relative '../config/environment.rb'
-
-## Read data on players
-JSON_Handler.read_existing_players
-
-game = Game.new()
-
-game.single_or_multi
-
-##Store data once game is over
-JSON_Handler.make_json
+# require_relative '../config/environment.rb'
+#
+# ## Read data on players
+# JSON_Handler.read_existing_players
+#
+# game = Game.new()
+#
+# game.single_or_multi
+#
+# ##Store data once game is over
+# JSON_Handler.make_json
